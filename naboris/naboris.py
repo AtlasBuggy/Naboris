5a3477ecb5564e5a2fb8270f496cbdf80e84a9b3
Woz4tetra <woz4tetra@gmail.com>
2017-04-28 04:52:51 +0000

    Added atlasbuggy and python control

---
+++ naboris/naboris.py
@@ -0,0 +1,20 @@
+
+from atlasbuggy.robot import Robot
+from atlasbuggy.interface import run, simulate
+
+from actuators import Actuators
+
+class Naboris(Robot):
+    def __init__(self):
+        self.actuators = Actuators()
+
+        super(Naboris, self).__init__(self.actuators)
+
+    # def start(self):
+    # def received(self):
+    # def loop(self):
+    # def close(self):
+
+naboris = Naboris()
+run(naboris, log_dir=None, log_data=False, debug_prints=True)
+simulate("22", "2017_Mar_02", naboris)
---
+++ python_launch_test.py
@@ -0,0 +1,4 @@
+
+with open("/home/pi/Desktop/something.txt", 'w+') as file:
+    file.write("I did a thing!")
+
