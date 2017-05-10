fce09fb3ed5dd2331b2ea4d7720938ea9e36076a
Woz4tetra <woz4tetra@gmail.com>
2017-05-10 18:23:45 -0400

    Fixed some bugs

--- naboris/plot_battery.py
+++ naboris/plot_battery.py
@@ -1,3 +1,4 @@
+from actuators import Actuators
 
 from atlasbuggy.interface.simulated import RobotSimulator
 from atlasbuggy.robot import Robot
@@ -14,15 +15,17 @@ class BatteryPlotter(Robot):
         self.battery_plot = RobotPlot("Battery plot")
         self.plotter = StaticPlotter(1, self.battery_plot)
 
-        super(Naboris, self).__init__(self.actuators)
+        super(BatteryPlotter, self).__init__(self.actuators)
 
-    def received(self):
-        self.battery_plot.append(self.actuators.value_V)
+    def received(self, timestamp, whoiam, packet, packet_type):
+        self.battery_plot.append(self.dt(), self.actuators.value_V)
 
-    def close(self):
+    def close(self, reason):
         self.plotter.close()
+
+battery_plotter = BatteryPlotter()
 
 file_name = "16;43;13"
 directory = "2017_May_10"
-simulator = RobotSimulator(file_name, directory, naboris, debug_enabled=False)
+simulator = RobotSimulator(file_name, directory, battery_plotter, debug_enabled=True)
 simulator.run()
