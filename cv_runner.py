acb346310f5843d640f02ef0aa62d5e3c7f50d83
Woz4tetra <woz4tetra@gmail.com>
2017-07-29 14:45:22 -0400

    Reorganized. Played some more with depth images. Moving texture classifier tests

---
+++ cv_runner.py
@@ -0,0 +1,161 @@
+from atlasbuggy import Robot
+from atlasbuggy.camera.video.videoplayer import VideoPlayer
+from atlasbuggy.camera.viewer.viewer import CameraViewer
+from atlasbuggy.subscriptions import *
+from atlasbuggy.plotters import LivePlotter, RobotPlot, RobotPlotCollection
+import asyncio
+
+# from naboris.masazi.pipeline import MasazIDepthPipeline
+from naboris.monodepth.pipeline import MonodepthPipeline
+
+
+# from naboris.pipeline import NaborisPipeline
+
+class MyCameraViewer(CameraViewer):
+    def __init__(self, enabled=True):
+        super(MyCameraViewer, self).__init__(enabled, draw_while_paused=True)
+
+        self.pipeline_tag = "pipeline"
+
+        self.pipeline = None
+        self.pipeline_feed = None
+
+        self.show_original = False
+
+        self.require_subscription(self.pipeline_tag, Update)
+
+    def take(self, subscriptions):
+        self.take_capture(subscriptions)
+        self.pipeline = subscriptions[self.pipeline_tag].get_stream()
+        self.pipeline_feed = subscriptions[self.pipeline_tag].get_feed()
+        self.set_feed()
+
+    def set_feed(self):
+        if self.show_original:
+            self.pipeline_feed.enabled = False
+            self.capture_feed.enabled = True
+        else:
+            self.pipeline_feed.enabled = True
+            self.capture_feed.enabled = False
+
+    def get_frame_from_feed(self):
+        if self.show_original:
+            return self.capture_feed.get()
+        else:
+            return self.pipeline_feed.get()
+
+    def key_down(self, key):
+        if key == 'o':
+            self.show_original = not self.show_original
+            self.set_feed()
+        elif key == 'q':
+            self.exit()
+        elif key == ' ':
+            self.toggle_pause()
+            if self.is_paused():
+                self.pipeline_feed.enabled = False
+                self.capture_feed.enabled = False
+            else:
+                self.pipeline_feed.enabled = True
+                self.capture_feed.enabled = True
+
+
+class DataPlotter(LivePlotter):
+    def __init__(self, enabled=True):
+        self.position_plot = RobotPlot("position_plot", marker=".", linestyle='-', color='red', enabled=False)
+
+        self.top_dist_plot = RobotPlot("top_dist_plot", color="blue")
+        self.left_dist_plot = RobotPlot("left_dist_plot", color="green")
+        self.right_dist_plot = RobotPlot("right_dist_plot", color="red")
+        self.distance_plot = RobotPlotCollection("dist_plot", self.top_dist_plot, self.left_dist_plot,
+                                                 self.right_dist_plot)
+
+        super(DataPlotter, self).__init__(2, self.position_plot, self.distance_plot, active_window_resizing=False,
+                                          enabled=enabled)
+
+        self.pipeline_tag = "pipeline"
+        self.results_service_tag = "results"
+        self.pipeline_feed = None
+        self.require_subscription(self.pipeline_tag, Feed, service_tag=self.results_service_tag)
+
+        max_limit = 900
+        self.x_limits = [-max_limit, max_limit]
+        self.y_limits = [-max_limit, max_limit]
+
+    def start(self):
+        self.get_axis(self.distance_plot).set_xlim(self.x_limits)
+        self.get_axis(self.distance_plot).set_ylim(self.y_limits)
+
+    def take(self, subscriptions):
+        self.pipeline_feed = subscriptions[self.pipeline_tag].get_feed()
+
+    async def update(self):
+        while not self.pipeline_feed.empty():
+            position, rotation, distances = await self.pipeline_feed.get()
+            self.pipeline_feed.task_done()
+
+            self.position_plot.append(position[0], position[1])
+
+            print(distances)
+            left_dist, right_dist, top_dist = distances
+
+            if left_dist is not None:
+                self.left_dist_plot.update([left_dist, left_dist], self.y_limits)
+                self.left_dist_plot.set_properties(color="blue")
+            else:
+                self.left_dist_plot.set_properties(color="gray")
+
+            if right_dist is not None:
+                self.right_dist_plot.update([right_dist, right_dist], self.y_limits)
+                self.right_dist_plot.set_properties(color="green")
+            else:
+                self.right_dist_plot.set_properties(color="gray")
+            if top_dist is not None:
+                self.top_dist_plot.update(self.x_limits, [top_dist, top_dist])
+                self.top_dist_plot.set_properties(color="red")
+            else:
+                self.top_dist_plot.set_properties(color="gray")
+
+        await asyncio.sleep(0.001)
+
+
+robot = Robot()
+
+# naboris videos
+# video_name = "videos/naboris/2017_Jul_14/22_36_21-1.mp4"  # my room 1
+# video_name = "videos/naboris/2017_Jul_14/23_24_32-3.mp4"  # my room 2
+# video_name = "videos/naboris/2017_May_28/15_37_43.mp4"  # on the desk
+video_name = "videos/naboris/2017_May_28/16_23_21.mp4"  # hallway
+# video_name = "videos/naboris/2017_Jul_16/23_03_26-1.mp4"  # running into a wall
+
+
+# buggy videos
+# video_name = "videos/cia_buggy_videos/Ascension 10-17 roll 3-2.mp4"
+# video_name = "videos/cia_buggy_videos/Icarus 10-11 roll 5 (+hill 1).mp4"
+# video_name = "videos/cia_buggy_videos/Impulse 2-21 roll 1.mp4"
+# video_name = "videos/rd17/2017_Apr_22/07_17_12rightcam.mp4"
+
+# rccar
+# video_name = "videos/rc_car/Sun 26 Jun 2016 14;54;35 EDT.avi"
+
+# other
+# video_name = "videos/Naboris Demo.mp4"
+# video_name = "videos/10_52_33.avi"
+
+capture = VideoPlayer(file_name=video_name, loop_video=True, enabled=True, width=640, height=400)
+
+# pipeline = MasazIDepthPipeline("depth_models/coarse", "depth_models/fine", enabled=True)
+# pipeline = NaborisPipeline(enabled=True)
+pipeline = MonodepthPipeline("kitti_resnet")
+
+# viewer = MyCameraViewer(enabled=True)
+viewer = CameraViewer(enabled=True)
+plotter = DataPlotter(enabled=False)
+
+# viewer.subscribe(Update(viewer.capture_tag, capture))
+viewer.subscribe(Update(viewer.capture_tag, pipeline))
+pipeline.subscribe(Update(pipeline.capture_tag, capture))
+pipeline.subscribe(Update(pipeline.viewer_tag, viewer))
+plotter.subscribe(Feed(plotter.pipeline_tag, pipeline, plotter.results_service_tag))
+
+robot.run(viewer, capture, pipeline, plotter)
---
+++ naboris/monodepth/pipeline.py
@@ -0,0 +1,133 @@
+import cv2
+import scipy.misc
+import numpy as np
+import tensorflow as tf
+
+from atlasbuggy.camera.pipeline import Pipeline
+from atlasbuggy.subscriptions import *
+
+from .model import monodepth_parameters, MonodepthModel
+
+
+def post_process_disparity(disp):
+    _, h, w = disp.shape
+    l_disp = disp[0, :, :]
+    r_disp = np.fliplr(disp[1, :, :])
+    m_disp = 0.5 * (l_disp + r_disp)
+    l, _ = np.meshgrid(np.linspace(0, 1, w), np.linspace(0, 1, h))
+    l_mask = 1.0 - np.clip(20 * (l - 0.05), 0, 1)
+    r_mask = np.fliplr(l_mask)
+    return r_mask * l_disp + l_mask * r_disp + (1.0 - l_mask - r_mask) * m_disp
+
+
+class MonodepthPipeline(Pipeline):
+    def __init__(self, model_name, enabled=True, log_level=None):
+        super(MonodepthPipeline, self).__init__(enabled, log_level)
+
+        self.results_service_tag = "results"
+        self.add_service(self.results_service_tag, lambda data: data)
+
+        self.viewer_tag = "viewer"
+        self.viewer_feed = None
+        self.require_subscription(self.viewer_tag, Update)
+
+        model_dir = "depth_models/monodepth/model_" + model_name
+
+        if model_name.endswith("resnet"):
+            encoder = "resnet50"
+        else:
+            encoder = "vgg"
+
+        self.input_height = 256
+        self.input_width = 512
+
+        params = monodepth_parameters(
+            encoder=encoder,
+            height=self.input_height,
+            width=self.input_width,
+            batch_size=2,
+            num_threads=1,
+            num_epochs=1,
+            do_stereo=False,
+            wrap_mode="border",
+            use_deconv=False,
+            alpha_image_loss=0,
+            disp_gradient_loss_weight=0,
+            lr_loss_weight=0,
+            full_summary=False
+        )
+
+        self.left = tf.placeholder(tf.float32, [2, self.input_height, self.input_width, 3])
+        self.model = MonodepthModel(params, "test", self.left, None)
+
+        config = tf.ConfigProto(allow_soft_placement=True)
+        self.sess = tf.Session(config=config)
+
+        # SAVER
+        train_saver = tf.train.Saver()
+
+        # INIT
+        self.sess.run(tf.global_variables_initializer())
+        self.sess.run(tf.local_variables_initializer())
+        coordinator = tf.train.Coordinator()
+        tf.train.start_queue_runners(sess=self.sess, coord=coordinator)
+
+        # RESTORE
+        train_saver.restore(self.sess, model_dir)
+
+    def take_from_pipeline(self, subscriptions):
+        self.viewer_feed = subscriptions[self.viewer_tag].get_feed()
+
+    def fit_frame(self, input_image):
+        original_height, original_width, num_channels = input_image.shape
+        if num_channels == 4:
+            input_image = input_image[:, :, :3]
+        elif num_channels == 1:
+            input_image = np.tile((input_image, input_image, input_image), 2)
+        input_image = scipy.misc.imresize(input_image, [self.input_height, self.input_width], interp='lanczos')
+        input_image = input_image.astype(np.float32) / 255
+        fitted_frame = np.stack((input_image, np.fliplr(input_image)), 0)
+        return fitted_frame
+
+    def pipeline(self, frame):
+        fitted_frame = self.fit_frame(frame)
+
+        disp = self.sess.run(self.model.disp_left_est[0], feed_dict={self.left: fitted_frame})
+        disp_pp = post_process_disparity(disp.squeeze()).astype(np.float32)
+
+        depth_image = scipy.misc.imresize(disp_pp.squeeze(), (self.height, self.width))
+
+        # depth_values = cv2.resize(disp_pp.squeeze(), (self.width, self.height))
+        # print((np.max(depth_values), np.min(depth_values)), (np.max(depth_values), np.min(depth_values)))
+
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(depth_image)
+
+        depth_image = cv2.cvtColor(depth_image, cv2.COLOR_GRAY2BGR)
+
+        cv2.circle(depth_image, min_loc, 5, (0, 255, 0))
+        cv2.circle(depth_image, max_loc, 5, (0, 0, 255))
+
+
+        # self.post(disp, self.results_service_tag)
+        offset = 50
+        pt1 = (self.width // 2 - offset, self.height // 2 - offset)
+        pt2 = (self.width // 2 + offset, self.height // 2 + offset)
+        cv2.rectangle(depth_image, pt1, pt2, (255, 0, 0))
+
+        # print(np.average(disp_pp[self.height // 2 - offset: self.height // 2 + offset,
+        #                  self.width // 2 - offset: self.width // 2 + offset]))
+        while not self.viewer_feed.empty():
+            event, x, y, flags, param = self.viewer_feed.get()
+            if 0 <= x < self.width and 0 <= y < self.height:
+                print("(%s, %s) -> %s" % (x, y, depth_image[y, x]))
+
+                # cv2.circle(depth_image, (x, y), 1, (0, 255, 0))
+                # cv2.circle(depth_image, (y, x), 1, (0, 0, 255))
+
+        return np.concatenate((depth_image, frame), axis=1)
+
+
+    def stop(self):
+        if self.sess is not None:
+            self.logger.debug('Close interactive session')
+            self.sess.close()
