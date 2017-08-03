833d37f79dd21a0566e3c86090a2a257dbfd65c0
Woz4tetra <woz4tetra@gmail.com>
2017-08-03 14:06:09 -0400

    Testing inception over socket. Experiencing network slowness

--- runner.py
+++ runner.py
@@ -10,8 +10,8 @@ from atlasbuggy.plotters import LivePlotter
 from naboris import Naboris
 from naboris.picamera import PiCamera
 from naboris.cli import NaborisCLI
-# from naboris.texture.pipeline import TexturePipeline
-from naboris.inception.pipeline import InceptionPipeline
+from naboris.texture.pipeline import TexturePipeline
+# from naboris.inception.pipeline import InceptionPipeline
 
 from naboris.site import NaborisWebsite
 from naboris.socket_server import NaborisSocketServer
@@ -20,7 +20,6 @@ from naboris.socket_server import NaborisSocketServer
 parser = argparse.ArgumentParser()
 parser.add_argument("-l", "--log", help="disable logging", action="store_false")
 parser.add_argument("-d", "--debug", help="enable debug prints", action="store_true")
-parser.add_argument("-pipe", "--nopipeline", help="disable pipeline", action="store_false")
 args = parser.parse_args()
 
 log = args.log
@@ -34,11 +33,10 @@ camera = PiCamera(file_name=video_file_name, directory=video_directory)
 naboris = Naboris()
 # pipeline = MasazIDepthPipeline("depth_models/coarse", "depth_models/fine", enabled=args.pipeline)
 # pipeline = CalibrationPipeline()
-# pipeline = TexturePipeline(args.nopipeline)
-pipeline = InceptionPipeline(args.nopipeline)
+pipeline = TexturePipeline(enabled=False)
+# pipeline = InceptionPipeline(enabled=True)
 cmdline = NaborisCLI()
 website = NaborisWebsite("templates", "static")
-socket = NaborisSocketServer(enabled=False)
 plotter = LivePlotter(1, close_when_finished=True, enabled=False)
 
 naboris.subscribe(Feed(naboris.pipeline_tag, pipeline, naboris.results_service_tag))
@@ -51,9 +49,6 @@ pipeline.subscribe(Update(pipeline.capture_tag, camera))
 website.subscribe(Subscription(website.cmd_tag, cmdline))
 website.subscribe(Update(website.camera_tag, camera, enabled=False))
 website.subscribe(Update(website.pipeline_tag, pipeline, enabled=False))
-website.subscribe(Subscription(website.plotter_tag, plotter))
+# website.subscribe(Subscription(website.plotter_tag, plotter))
 
-socket.subscribe(Update(socket.camera_tag, camera, enabled=False))
-socket.subscribe(Subscription(socket.cmdline_tag, cmdline))
-
-robot.run(camera, naboris, pipeline, cmdline, website, socket, plotter)
+robot.run(camera, naboris, pipeline, cmdline, website)
